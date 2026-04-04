689e4fe1710269fcc908802e13fd546466a1aa2e
ZUSIPHE74 <sidimbazusiphe@gmail.com>
2026-04-04 10:57:32 +0200

    Remove duplicate news_system project copy

--- news_system/news_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news_app'
-
-    def ready(self):
-        import news_app.signals
